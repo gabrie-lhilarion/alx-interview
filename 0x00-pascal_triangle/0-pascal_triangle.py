3d53bdf5e4a7fcbbdb226a41ecad956330f3421f
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-06-28 01:32:49 +0100

    Add docstrings

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
 
+"""
+This script defines a function to generate Pascal's triangle up to n rows.
+
+Usage:
+    You can run this script directly to see Pascal's triangle printed for a specific value of n.
+
+Example:
+    $ python3 pascal_triangle.py
+    [
+        [1],
+        [1, 1],
+        [1, 2, 1],
+        [1, 3, 3, 1],
+        [1, 4, 6, 4, 1]
+    ]
+"""
+
 def pascal_triangle(n):
     """
     Generates Pascal's triangle of size n.
@@ -12,15 +29,7 @@ def pascal_triangle(n):
         representing Pascal's triangle.
         Each sublist represents a row in the triangle.
 
-    Example:
-        >>> pascal_triangle(5)
-        [
-            [1],
-            [1, 1],
-            [1, 2, 1],
-            [1, 3, 3, 1],
-            [1, 4, 6, 4, 1]
-        ]
+
     """
     if n <= 0:
         return []
