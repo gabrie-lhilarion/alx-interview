36feb78c384873af2e1460dd367fc21613689250
gabrie-lhilarion <gabrielhilarion@gmail.com>
2024-07-05 01:40:53 +0100

    Lockboxes add read me file correction

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,21 +3,22 @@ def canUnlockAll(boxes):
     Determines if all boxes can be opened.
 
     Args:
-        boxes (list of lists): List of lists where each sublist represents keys contained in a box.
+        boxes (list of lists): List of lists where each sublist
+        represents keys contained in a box.
 
     Returns:
         bool: True if all boxes can be opened, False otherwise.
     """
-    n = len(boxes)  # Total number of boxes
-    visited = [False] * n  # List to track which boxes have been opened
-    visited[0] = True  # The first box is initially unlocked
-    stack = [0]  # Stack to manage the boxes to be explored, starting with the first box
+    n = len(boxes)
+    visited = [False] * n
+    visited[0] = True
+    stack = [0]
 
     while stack:
-        current_box = stack.pop()  # Get a box to explore
-        for key in boxes[current_box]:  # Iterate through the keys in the current box
-            if key < n and not visited[key]:  # Check if the key opens a valid and yet unopened box
-                visited[key] = True  # Mark the box as opened
-                stack.append(key)  # Add the box to the stack for further exploration
+        current_box = stack.pop()
+        for key in boxes[current_box]:
+            if key < n and not visited[key]:
+                visited[key] = True
+                stack.append(key)
 
-    return all(visited)  # Check if all boxes have been visited (opened)
+    return all(visited)
